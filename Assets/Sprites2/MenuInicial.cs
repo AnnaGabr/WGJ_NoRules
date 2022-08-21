0911680d1a958c4d3a11efbca31137b164086a36
Anna <annagabriela2940@gmail.com>
2022-08-21 10:38:55 -0500

    fix(menuinicio): ya es funcional

--- Assets/Sprites2/MenuInicial.cs
+++ Assets/Sprites2/MenuInicial.cs
@@ -8,7 +8,7 @@ public class MenuInicial : MonoBehaviour
     // Start is called before the first frame update
     public void Jugar()
     {
-        SceneManager.LoadScene(SceneManaget.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     // Update is called once per frame
